cc2eb3c761c69d575a70b68cacaba2bfcf7b5a66
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-03-29 21:30:59 +0100

    fehler suche gazebo läd nicht richtig

--- aster_description/launch/gazebo.launch.py
+++ aster_description/launch/gazebo.launch.py
@@ -11,6 +11,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+    
     aster_description_dir = get_package_share_directory('aster_description')
     aster_description_share = os.path.join(get_package_prefix('aster_description'), 'share')
     gazebo_ros_dir = get_package_share_directory('gazebo_ros')
